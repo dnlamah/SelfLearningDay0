85be20b2dd38626a90cc454a1ecac018680b5556
dnlamah <dnlamah1@gmail.com>
2017-04-08 10:41:43 +0100

    Add testcase 9 and 10 for fizztest

--- tests/fizztest.js
+++ tests/fizztest.js
@@ -12,9 +12,11 @@ expect(myApp.fizzBuzz(3)).toBe('Fizz');
  });  
  it("should return `FizzBuzz` for 45", function() {    
  expect(myApp.fizzBuzz(45)).toBe('FizzBuzz'); 
- });  it("should return `FizzBuzz` for 90", function() {    
+ });  
+ it("should return `FizzBuzz` for 90", function() {    
  expect(myApp.fizzBuzz(90)).toBe('FizzBuzz');  
- });  it("should return `Fizz` for 63", function() {    
+ }); 
+ it("should return `Fizz` for 63", function() {    
  expect(myApp.fizzBuzz(63)).toBe('Fizz');  });  
  it("should return 7 since its indivisible by 3 and 5", function() {  
  expect(myApp.fizzBuzz(7)).toBe(7); 
@@ -22,4 +24,10 @@ expect(myApp.fizzBuzz(3)).toBe('Fizz');
  it("should return 101 since its indivisible by 3 and 5", function() {   
  expect(myApp.fizzBuzz(101)).toBe(101); 
  }); 
+it("should return 104 since its indivisible by 3 and 5", function() {
+	expect(myApp.fizzBuzz(104)).toBe(104);
+})
+it("should return 109 since its indivisible by 3 and 5", function() {
+	expect(myApp.fizzBuzz(109)).toBe(109);
+})
  });
