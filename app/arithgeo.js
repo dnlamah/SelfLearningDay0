48443e0e20cbf0117b06cfd458f2f62212a96700
dnlamah <dnlamah1@gmail.com>
2017-04-08 11:29:45 +0100

    add provision for return 0

--- app/arithgeo.js
+++ app/arithgeo.js
@@ -10,6 +10,7 @@ module.exports = {
 var diff = arr[1] - arr[0];
 var ratio = arr[1] / arr[0];
 
+
 var arith = true;
 var geo = true;
 
@@ -19,14 +20,17 @@ for(var i = 0; i < arr.length - 1; i++)
       arith = false;
     if(arr[i + 1] / ratio !== arr[i])
       geo = false;
+    
 }
 
 if(arith === true)
     return "Arithmetic";
+
 else if(geo === true)
     return "Geometric";
 else
     return -1;
+
 
 }
 
